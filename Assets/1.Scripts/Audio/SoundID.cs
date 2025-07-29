0bb1e6547f3cae9fff821826a5fa959f09b9f3be
kim000420 <kim000420@naver.com>
2025-07-30 00:34:08 +0900

    sfx SO생성 및 정리 (추가)

--- Assets/1.Scripts/Audio/SoundID.cs
+++ Assets/1.Scripts/Audio/SoundID.cs
@@ -2,26 +2,40 @@
 public enum SoundID
 {
     // 필요에 따라 추가
-    sfx_timo,
-    sfx_attack_arrow,
-    sfx_player_walk,
-    sfx_player_guard,
-    sfx_player_attack_01,
-    sfx_player_attack_02,
+    sfx_timo, // 티모 푝푝!
+    sfx_attack_arrow, // 화살 푝!
+    sfx_player_walk, // 걷기
+    sfx_player_guard, // 가드 성공
+    sfx_player_attack_01, // 공격 히트성공
+    sfx_player_attack_02, 
     sfx_player_attack_03,
     sfx_player_attack_04,
-    sfx_player_roll,
-    sfx_npc_talk,
-    sfx_getitem,
-    sfx_dore_open,
-    sfx_critical_01,
+    sfx_player_roll, // 구르기
+    sfx_player_landing, // 착지
+    sfx_npc_talk, // 대화 사운드
+    sfx_getitem, // 아이템 줍줍 사운드
+    sfx_dore_open, // 문여는소리
+    sfx_critical_01, // 크리티컬
     sfx_critical_02,
-    sfx_change_scean,
-    bgm_title,
-    bgm_mepi,
-    bgm_jo,
-    bgm_home,
-    bgm_halbe,
-    bgm_fight_jo,
-    bgm_fight_boss
+    sfx_change_scean, // 씬변경
+    sfx_swing_01, // 둔탁한 휘둘
+    sfx_swing_02,
+    sfx_swing_03,
+    sfx_swing_sting, // 찌르기
+    sfx_swing_11, // 날카로운 휘둘
+    sfx_swing_12,
+    sfx_swing_13,
+    sfx_swing_14,
+    sfx_swing_strong_01, // 무거운 휘둘
+    sfx_swing_strong_02,
+    sfx_swing_strong_03,
+    sfx_gun_reload, // 총 재장전
+    sfx_gun_fire, // 총 발사
+    bgm_title, // 타이틀 브금
+    bgm_mepi, // 매피첫만남 브금
+    bgm_jo, // 죠 브금
+    bgm_home, // 집 브금
+    bgm_halbe, // 할배리스시티 브금
+    bgm_fight_jo, // 죠 싸울때 브금
+    bgm_fight_boss // 보스와 싸울때 브금
 }
